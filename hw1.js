ddf32e2539b0a221de8a027a70d9560047b3c274
safa0824 <safa0824@naver.com>
2020-02-14 14:15:15 +0900

    update

--- hw1.js
+++ hw1.js
@@ -7,22 +7,22 @@ class App extends Component{
     const { name, value } = event.target;
     this.setState({
       [name]: value
-    });
-    // const passLeng = event.target.value.length;
-    // passLeng > 6 ? console.log( passLeng+" - 여섯자리됨") : console.log( passLeng+" - 여섯자리안됨")
-    // console.log( userPass.indexOf(userName) ) 
-  };    
-  isSafe = password => {
-    // username을 포함하고 있지 않을 때는 어떻게 적용을 시켜야할지 모르겠습니다 ㅜㅜ 
+    });  
   }
+
+  isSafe = password => {
+    
+
+  };  
+
   render(){
-      const { userName, password } = this.state;
+      const { username, password } = this.state;
       return(
-          <form onSubmit="">
+          <form>
             <div className="input_row">
-                <input type="text" name="userName" value={ userName } />
+                <input type="text" name="username" value={ username } onChange={this.handleChange}/>
                 <input type="password" name="password" value={ password } onChange={this.handleChange}/>
-                <button disabled={ password.length >= 6  ? null : "disabled" } name="loginBtn" type="submit">로그인</button>
+                <button disabled={ (password.length >= 6 ) && ( password.indexOf(username) < 0 ) ? null : "disabled" } name="loginBtn" type="submit">로그인</button>
             </div>
           </form>
       )
